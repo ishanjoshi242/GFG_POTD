38aef62f70bad1398b9ca214810e60073fbf94d1
Ishan Joshi <120934357+ishanjoshi242@users.noreply.github.com>
2023-12-02 09:26:47 +0530

    Create 02-12-2023.cpp
    Inorder traversal and BST.

---
+++ 02-12-2023.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        for(int i = 1; i < N; i++){
+            if(arr[i] <= arr[i - 1])
+                return 0;
+        }
+        
+        return 1;
+    }
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i=0;i<N;i++)cin>>arr[i];
+        Solution ob;
+        cout<<ob.isRepresentingBST(arr,N)<<endl;
+    }
+    return 0;
+}
